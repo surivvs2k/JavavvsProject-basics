aad8c1f82a19f0608e2c72e2149e807cbdd76bdf
surivvs2k <111076850+surivvs2k@users.noreply.github.com>
2023-03-09 06:16:36 +0530

    Update UncheckedExceptions.java

--- src/exceptionshandling/UncheckedExceptions.java
+++ src/exceptionshandling/UncheckedExceptions.java
@@ -25,3 +25,11 @@ public class UncheckedExceptions {
 	}
 
 }
+
+
+/*
+Output:
+Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 10
+	at exceptionshandling.UncheckedExceptions.main(UncheckedExceptions.java:22)
+
+*/
